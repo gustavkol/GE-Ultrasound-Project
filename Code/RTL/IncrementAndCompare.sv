a55487cf0230f292e0327562cbf3f4ef91947008
gustavkol <gustavko@stud.ntnu.no>
2023-11-07 21:34:07 +0100

    IncrementAndCompare working with pos/neg inputs

--- Code/RTL/IncrementAndCompare.sv
+++ Code/RTL/IncrementAndCompare.sv
@@ -3,7 +3,7 @@ module IncrementAndCompare  #(
                             parameter INC_TERM_DW_INTEGER = 16,
                             parameter N_DW_INTEGER = 13,
                             parameter ERROR_DW_INTEGER = 14,
-                            parameter A_DW_INTEGER = 3,
+                            parameter A_DW_INTEGER = 4,
                             parameter DW_FRACTIONAL = 4
                             )(
                             input                                                   clk,            // Clock signal
@@ -28,18 +28,20 @@ module IncrementAndCompare  #(
 
     // Constant step size parameter
     localparam [2:0] stepsize = 3'b100; 
+    localparam signed [4:0] increment = 4'b0001; 
 
     // State machine variables
     enum {LOAD, RUN, WAIT, IDLE} state, nextState;
 
     // Algorithm registers
     logic signed [INC_TERM_DW_INTEGER+DW_FRACTIONAL:0]      comp_term_cur_reg;
-    logic [N_DW_INTEGER+DW_FRACTIONAL:0]                    n_cur_reg;
+    logic signed [INC_TERM_DW_INTEGER+DW_FRACTIONAL:0]      comp_term_next_reg;
+    logic signed [N_DW_INTEGER+DW_FRACTIONAL:0]             n_cur_reg;
     logic signed [ERROR_DW_INTEGER+DW_FRACTIONAL:0]         error_cur_reg;
     logic signed [A_DW_INTEGER+DW_FRACTIONAL:0]             a_cur_reg;
     logic signed [A_DW_INTEGER+DW_FRACTIONAL:0]             a_prev_reg;
-    logic [2*A_DW_INTEGER+DW_FRACTIONAL:0]                  a_cur_sq_reg;
-    logic [2*A_DW_INTEGER+DW_FRACTIONAL:0]                  a_prev_sq_reg;                                              
+    logic signed [2*A_DW_INTEGER+DW_FRACTIONAL:0]           a_prev_sq_reg;
+    logic signed [2*A_DW_INTEGER+DW_FRACTIONAL:0]           a_cur_sq_reg;
     logic                                                   sign_bit;
     logic signed [21:0]                                     a_mul_n_integer_reg;        
     logic signed [19:0]                                     a_mul_n_fractional_reg;     // Additional sign bit and addition extension
@@ -55,12 +57,15 @@ module IncrementAndCompare  #(
     assign error_next       = (state == WAIT) ? error_cur_reg       : '0;
     assign a_next           = (state == WAIT) ? a_cur_reg           : '0;
     assign a_next_sq        = (state == WAIT) ? a_cur_sq_reg        : '0;
-    assign comp_term_next   = (state == WAIT) ? comp_term_cur_reg   : '0;
+    assign comp_term_next   = (state == WAIT) ? comp_term_next_reg  : '0;
     assign ready            = (state == WAIT) ? ready_reg           : '0;
 
     // Sum of increment term in comparator (should work as a wire)
     logic signed [INC_TERM_DW_INTEGER+DW_FRACTIONAL:0]      comparator_sum;
-    assign comparator_sum = a_mul_n_integer_reg + a_mul_n_fractional_reg + a_prev_sq_reg + inc_term_reg;
+    logic signed [A_DW_INTEGER+DW_FRACTIONAL:0]             a_prev_shifted, a_cur_shifted;
+    assign comparator_sum = a_mul_n_integer_reg + a_mul_n_fractional_reg + a_prev_sq_reg + inc_term_reg; // current 2*a*N + a^2
+    assign a_prev_shifted = (a_prev_reg >>> 1);
+    assign a_cur_shifted  = (a_cur_reg >>> 1);
 
     // Locking next state
     always @(posedge clk) begin
@@ -91,32 +96,39 @@ module IncrementAndCompare  #(
     always @(posedge clk) begin
         if (rst) begin
             comp_term_cur_reg           <= '0;
+            comp_term_next_reg          <= '0;
             n_cur_reg                   <= '0;
             error_cur_reg               <= '0;
             a_cur_reg                   <= '0;
             a_prev_reg                  <= '0;
             a_cur_sq_reg                <= '0;
+            a_prev_sq_reg               <= '0;
+            sign_bit                    <= '0;
             a_mul_n_integer_reg         <= '0;
             a_mul_n_fractional_reg      <= '0;
             inc_term_reg                <= '0;
-            ready_reg                   <= 1'b0;
-            sign_bit                    <= 1'b0;
+            comparator_sum_prev_reg     <= '0;
+            counter                     <= '0;
+            ready_reg                   <= '0;
         end
         else begin
             case(state)
                 IDLE: begin
                     comp_term_cur_reg           <= '0;
+                    comp_term_next_reg          <= '0;
                     n_cur_reg                   <= '0;
                     error_cur_reg               <= '0;
                     a_cur_reg                   <= '0;
                     a_prev_reg                  <= '0;
                     a_cur_sq_reg                <= '0;
+                    a_prev_sq_reg               <= '0;
+                    sign_bit                    <= '0;
                     a_mul_n_integer_reg         <= '0;
                     a_mul_n_fractional_reg      <= '0;
                     inc_term_reg                <= '0;
-                    ready_reg                   <= 1'b0;
-                    sign_bit                    <= 1'b0;
-		    counter			<= 4'b0000;
+                    comparator_sum_prev_reg     <= '0;
+                    counter                     <= '0;
+                    ready_reg                   <= '0;
                 end
                 LOAD: begin
                     n_cur_reg           <= n_prev;
@@ -125,20 +137,39 @@ module IncrementAndCompare  #(
                     a_cur_sq_reg        <= a_prev_sq;
                     a_prev_sq_reg       <= a_prev_sq;
                     comp_term_cur_reg   <= comp_term + error_prev;
-                    case(a_prev[5:4])    // Calculating integer part of 2*N_prev*a_prev
-                        2'b00:   a_mul_n_integer_reg <= 0;
-                        2'b01:   a_mul_n_integer_reg <= n_prev << 1;                 // 2*N_prev
-                        2'b10:   a_mul_n_integer_reg <= n_prev << 2;                 // 4*N_prev = 2^2*N_prev
-                        2'b11:   a_mul_n_integer_reg <= (n_prev << 1) + (n_prev << 2);         // 6*N_prev = 2^2*N_prev + 2*N_prev
+                    // TODO : FILL WITH 1's WHEN A < 0
+                    case(a_prev[6:4])    // Calculating integer part of 2*N_prev*a_prev
+                        3'b000:     a_mul_n_integer_reg <= 0;     
+                        3'b001:     a_mul_n_integer_reg <= (n_prev << 1);                   //2*N_prev
+                        3'b010:     a_mul_n_integer_reg <= (n_prev << 2);                   //4*N_prev           
+                        3'b011:     a_mul_n_integer_reg <= (n_prev << 1) + (n_prev << 2);   //6*N_prev     
+                        3'b100: begin
+                            if(a_prev[A_DW_INTEGER+DW_FRACTIONAL] == 1'b1)  a_mul_n_integer_reg <= -(n_prev << 3);  // -8*N_prev = -2^3*N_prev
+                            else                                            a_mul_n_integer_reg <= (n_prev << 3);   // 8*N_prev = 2^3*N_prev
+                        end
+                        3'b101:     a_mul_n_integer_reg <= -(n_prev << 1) - (n_prev << 2);  //-6*N_prev
+                        3'b110:     a_mul_n_integer_reg <= -(n_prev << 2);                  //-4*N_prev
+                        3'b111:     a_mul_n_integer_reg <= -(n_prev << 1);                  //-2*N_prev
+                        default:;  // Should never happen
                     endcase
                     case(a_prev[3:2])               // Calculating fractional part of 2*N_prev*a_prev (step size = 1/4)
                         2'b00:   a_mul_n_fractional_reg <= 0;
-                        2'b01:   a_mul_n_fractional_reg <= n_prev;                   // N_prev
-                        2'b10:   a_mul_n_fractional_reg <= n_prev >> 1;              // (1/2)*N_prev
-                        2'b11:   a_mul_n_fractional_reg <= n_prev + (n_prev >> 1);           // N_prev + (1/2)*N_prev
+                        2'b01: begin
+                            if(a_prev[A_DW_INTEGER+DW_FRACTIONAL] == 1'b1)  a_mul_n_fractional_reg <= - n_prev - (n_prev >> 1);     // -0.75*2*N_prev
+                            else                                            a_mul_n_fractional_reg <= (n_prev >> 1);                // 0.25*2*N_prev
+                        end
+                        2'b10: begin
+                            if(a_prev[A_DW_INTEGER+DW_FRACTIONAL] == 1'b1)  a_mul_n_fractional_reg <= -n_prev;                      // -0.5*2*N_prev
+                            else                                            a_mul_n_fractional_reg <= n_prev;                       // 0.5*2*N_prev
+                        end
+                        2'b11: begin
+                            if(a_prev[A_DW_INTEGER+DW_FRACTIONAL] == 1'b1)  a_mul_n_fractional_reg <= -(n_prev >> 1);               // -0.25*2*N_prev
+                            else                                            a_mul_n_fractional_reg <= n_prev + (n_prev >> 1);       // 0.75*2*N_prev
+                        end  
                     endcase
-                    if (comp_term + error_prev < comp_term_prev)
-                        sign_bit <= 1;      // -1
+                    if (comp_term + error_prev < comp_term_prev) begin
+                        sign_bit                    <= 1;      // -1
+                    end
                 end
                 RUN: begin
 		            counter <= counter + 1;
@@ -147,16 +178,17 @@ module IncrementAndCompare  #(
                         if(comparator_sum < comp_term_cur_reg) begin
                             n_cur_reg           <= n_cur_reg + a_cur_reg + stepsize;
                             a_cur_reg           <= a_cur_reg + stepsize;
-                            a_cur_sq_reg        <= a_cur_sq_reg + (a_cur_reg >> 1)  + (4'b0001);
+                            a_cur_sq_reg        <= a_cur_sq_reg + a_cur_shifted + increment;
                             error_cur_reg       <= comp_term_cur_reg - comparator_sum_prev_reg;
+                            comp_term_next_reg  <= comparator_sum_prev_reg;
                             ready_reg           <= 1'b1;
                         end
                         else begin   
                             // Updating incremented terms
-                            inc_term_reg <= inc_term_reg - (n_cur_reg >> 1) - (a_prev_reg >> 1) + 4'b0001 - ({counter,4'd0} >> 3);
-			                comparator_sum_prev_reg <= comparator_sum;
+                            comparator_sum_prev_reg <= comparator_sum;
                             a_cur_reg    <= a_cur_reg - stepsize;
-                            a_cur_sq_reg <= a_cur_sq_reg - (a_cur_reg >> 1)  + (4'b0001);
+                            inc_term_reg <= inc_term_reg - (n_cur_reg >> 1) - a_prev_shifted + increment - signed'({counter,4'd0} >> 3);
+                            a_cur_sq_reg <= a_cur_sq_reg - a_cur_shifted + increment;
                         end
 
                     // Comparator: 2*a*N_prev+a^2 > comp_term_w_error
@@ -164,16 +196,17 @@ module IncrementAndCompare  #(
                         if(comparator_sum > comp_term_cur_reg) begin
                             n_cur_reg           <= n_cur_reg + a_cur_reg - stepsize;
                             a_cur_reg           <= a_cur_reg - stepsize;
-                            a_cur_sq_reg        <= a_cur_sq_reg - (a_cur_reg >> 1) + (4'b0001);
+                            a_cur_sq_reg        <= a_cur_sq_reg - a_cur_shifted + increment;
                             error_cur_reg       <= comp_term_cur_reg - comparator_sum_prev_reg;
+                            comp_term_next_reg  <= comparator_sum_prev_reg;
                             ready_reg           <= 1'b1;
                         end
                         else begin   
                             // Updating incremented terms
-                            inc_term_reg <= inc_term_reg + (n_cur_reg >> 1) + (a_prev_reg >> 1) + 4'b0001 + ({counter,4'd0} >> 3);
 			                comparator_sum_prev_reg <= comparator_sum;
                             a_cur_reg    <= a_cur_reg + stepsize;
-                            a_cur_sq_reg <= a_cur_sq_reg + (a_cur_reg >> 1)  + (4'b0001);
+                            inc_term_reg <= inc_term_reg + (n_cur_reg >> 1) + a_prev_shifted + increment + signed'({counter,4'd0} >> 3);
+                            a_cur_sq_reg <= a_cur_sq_reg + a_cur_shifted + increment;
                         end
                 end
             endcase
